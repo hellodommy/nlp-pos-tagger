f0f87202c544357c75d7a677c91b86ca898b45e5
Dominique Ng <dominique@u.nus.edu>
2021-09-12 02:05:24 +0800

    Count for words, tags and word tags

--- buildtagger.py
+++ buildtagger.py
@@ -5,9 +5,39 @@ import math
 import sys
 import datetime
 
+WORD_COUNTS = 'WORD_COUNTS'
+TAG_COUNTS = 'TAG_COUNTS'
+TAG_BICOUNTS = 'TAG_BICOUNTS' # P(t(i-1), t(i))
+WORD_TAG_COUNTS = 'WORD_TAG_COUNTS'
+END_TAG_COUNTS = 'END_TAG_COUNTS' # P(</s>|tT)
+
+data = {}
+data[WORD_COUNTS] = {}
+data[TAG_COUNTS] = {}
+data[TAG_BICOUNTS] = {}
+data[WORD_TAG_COUNTS] = {}
+data[END_TAG_COUNTS] = {}
 
 def train_model(train_file, model_file):
     # write your code here. You can add functions as well.
+
+    with open(train_file) as f:
+        lines = f.readlines();
+        for line in lines:
+            words_with_tags = line.split(' ')
+            words = [word_with_tag.rsplit('/', 1)[0].lower()
+                     for word_with_tag in words_with_tags]
+            tags = [word_with_tag.rsplit('/', 1)[1]
+                     for word_with_tag in words_with_tags]
+            for i in range(len(words_with_tags) - 1):
+                data[WORD_COUNTS][words[i]] = data[WORD_COUNTS].get(words[i], 0) + 1
+                data[TAG_COUNTS][tags[i]] = data[TAG_COUNTS].get(tags[i], 0) + 1
+                if words[i] in data[WORD_TAG_COUNTS]:
+                    data[WORD_TAG_COUNTS][words[i]][tags[i]] = data[WORD_TAG_COUNTS][words[i]].get(tags[i], 0) + 1
+                else:
+                    data[WORD_TAG_COUNTS][words[i]] = {}
+                    data[WORD_TAG_COUNTS][words[i]][tags[i]] = 1
+        print(data[WORD_TAG_COUNTS])
     print('Finished...')
 
 if __name__ == "__main__":
