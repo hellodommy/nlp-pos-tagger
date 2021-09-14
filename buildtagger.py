81df619407bbfdf48ef4767006760f8ffdde8287
Dominique Ng <dominique@u.nus.edu>
2021-09-14 17:23:30 +0800

    Redo approach for saving end tag bicount (save under the prev tag instead of own end tag list)

--- buildtagger.py
+++ buildtagger.py
@@ -8,12 +8,12 @@ import json
 
 WORD_COUNTS = 'WORD_COUNTS'
 TAG_COUNTS = 'TAG_COUNTS'
-TAG_BICOUNTS = 'TAG_BICOUNTS' # P(t(i-1), t(i))
+TAG_TAG_COUNTS = 'TAG_TAG_COUNTS' # P(t(i-1), t(i))
 WORD_TAG_COUNTS = 'WORD_TAG_COUNTS'
 WORD_TAG_PROBS = 'WORD_TAG_PROBS'
-TAG_BICOUNT_PROBS = 'TAG_BICOUNT_PROBS'
-START_TAG = '<s>'
-END_TAG = '</s>'
+TAG_TAG_PROBS = 'TAG_TAG_PROBS'
+START_TAG = '<start>'
+END_TAG = '</end>'
 UNK = '<UNK>'
 TAGS = {
     '``': 0,
@@ -67,11 +67,10 @@ NUM_PENN_TAGS = len(TAGS)
 data = {}
 data[WORD_COUNTS] = {}
 data[TAG_COUNTS] = {}
-data[TAG_BICOUNTS] = {}
+data[TAG_TAG_COUNTS] = {}
 data[WORD_TAG_COUNTS] = {}
-data[TAG_BICOUNTS][START_TAG] = {}
-data[TAG_BICOUNTS][END_TAG] = {}
-data[TAG_BICOUNT_PROBS] = {}
+data[TAG_TAG_COUNTS][START_TAG] = {}
+data[TAG_TAG_PROBS] = {}
 data[WORD_TAG_PROBS] = {}
 
 def train_model(train_file, model_file):
@@ -86,54 +85,28 @@ def train_model(train_file, model_file):
             tags = [word_with_tag.rsplit('/', 1)[1]
                      for word_with_tag in words_with_tags]
             for i in range(len(words_with_tags)):
+                # remove WORD_COUNTS if not needed
                 data[WORD_COUNTS][words[i]] = data[WORD_COUNTS].get(words[i], 0) + 1
                 data[TAG_COUNTS][tags[i]] = data[TAG_COUNTS].get(tags[i], 0) + 1
                 if i == 1:
-                    data[TAG_BICOUNTS][START_TAG][tags[i]] = data[TAG_BICOUNTS][START_TAG].get(tags[i], 0) + 1
+                    data[TAG_TAG_COUNTS][START_TAG][tags[i]] = data[TAG_TAG_COUNTS][START_TAG].get(tags[i], 0) + 1
                 if words[i] in data[WORD_TAG_COUNTS]:
                     data[WORD_TAG_COUNTS][words[i]][tags[i]] = data[WORD_TAG_COUNTS][words[i]].get(tags[i], 0) + 1
                 else:
                     data[WORD_TAG_COUNTS][words[i]] = {}
                     data[WORD_TAG_COUNTS][words[i]][tags[i]] = 1
                 if i != len(words_with_tags) - 1: # if not last word
-                    if tags[i] in data[TAG_BICOUNTS]:
-                        data[TAG_BICOUNTS][tags[i]][tags[i + 1]] = data[TAG_BICOUNTS][tags[i]].get(tags[i + 1], 0) + 1
+                    if tags[i] in data[TAG_TAG_COUNTS]:
+                        data[TAG_TAG_COUNTS][tags[i]][tags[i + 1]] = data[TAG_TAG_COUNTS][tags[i]].get(tags[i + 1], 0) + 1
                     else:
-                        data[TAG_BICOUNTS][tags[i]] = {}
-                        data[TAG_BICOUNTS][tags[i]][tags[i + 1]] = 1
+                        data[TAG_TAG_COUNTS][tags[i]] = {}
+                        data[TAG_TAG_COUNTS][tags[i]][tags[i + 1]] = 1
                 else: # is last word
-                    data[TAG_BICOUNTS][END_TAG][tags[i]] = data[TAG_BICOUNTS][END_TAG].get(tags[i], 0) + 1
-        data[TAG_COUNTS][START_TAG] = len(lines)
-        data[TAG_COUNTS][END_TAG] = len(lines)
-        # add-one smoothing for tag bigrams
-        # assumes first_tag includes all tags
-        # todo: add unseen first_tag
-        for first_tag in data[TAG_BICOUNTS]: # seen tag bigrams
-            data[TAG_BICOUNT_PROBS][first_tag] = {}
-            for second_tag in data[TAG_BICOUNTS][first_tag]:
-                if first_tag == END_TAG:
-                    data[TAG_BICOUNT_PROBS][first_tag][second_tag] = math.log(((data[TAG_BICOUNTS][first_tag][second_tag] + 1)/(data[TAG_COUNTS][second_tag] + NUM_PENN_TAGS)), 10)
-                else:
-                    data[TAG_BICOUNT_PROBS][first_tag][second_tag] = math.log(((data[TAG_BICOUNTS][first_tag][second_tag] + 1)/(data[TAG_COUNTS][first_tag] + NUM_PENN_TAGS)), 10)
-        for first_tag in data[TAG_BICOUNTS]: # unseen tag bigrams
-            for second_tag in TAGS:
-                if second_tag not in data[TAG_BICOUNTS][first_tag]:
-                    if first_tag == END_TAG:
-                        data[TAG_BICOUNT_PROBS][first_tag][second_tag] = math.log(((1)/(data[TAG_COUNTS][first_tag] + NUM_PENN_TAGS)), 10)              
+                    if tags[i] in data[TAG_TAG_COUNTS]:
+                        data[TAG_TAG_COUNTS][tags[i]][END_TAG] = data[TAG_TAG_COUNTS][tags[i]].get(END_TAG, 0) + 1
                     else:
-                        data[TAG_BICOUNT_PROBS][first_tag][second_tag] = math.log(((1)/(data[TAG_COUNTS][first_tag] + NUM_PENN_TAGS)), 10)
-        # add-one smoothing for word-tag bigrams
-        for word in data[WORD_COUNTS]: # seen word-tags
-            data[WORD_TAG_PROBS][word] = {}
-            for tag in TAGS:
-                if tag in data[WORD_TAG_COUNTS][word]:
-                    data[WORD_TAG_PROBS][word][tag] = math.log(((data[WORD_TAG_COUNTS][word][tag] + 0.1)/(data[TAG_COUNTS][tag] + 0.1*NUM_PENN_TAGS)), 10)
-                else:
-                    # todo: fix this
-                    data[WORD_TAG_PROBS][word][tag] = math.log((0.1/(data[TAG_COUNTS][tag] + 0.1*NUM_PENN_TAGS)), 10)
-        data[WORD_TAG_PROBS][UNK] = {} # unseen word-tags
-        for tag in TAGS:
-            data[WORD_TAG_PROBS][UNK][tag] = math.log((1/(data[TAG_COUNTS][tag] + NUM_PENN_TAGS)), 10)
+                        data[TAG_TAG_COUNTS][tags[i]] = {}
+                        data[TAG_TAG_COUNTS][tags[i]][END_TAG] = 1
 
     with open(model_file, 'w') as wf:
         json_obj = json.dumps(data, indent=2)
